1168f55a5dd72f640bf1b4fa4e1f173934caa7d7
ThomasCattarossiD <pro.thomascd@gmail.com>
2025-11-05 10:33:07 +0100

    Add About Me page and enhance navigation in layout

---
+++ src/app/about/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import { Container } from 'react-bootstrap';
+
+export default function AboutMe() {
+  return (
+    <Container className="py-5">
+      <h1 className="fw-bold mb-4">About Me</h1>
+      <p>
+        This is the About Me page. You can edit this text to write a description about yourself or the project.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </Container>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 'use client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -15,6 +15,13 @@ export default function RootLayout({ children }) {
         <Navbar bg="light" expand="lg" className="shadow-sm">
           <Container>
             <Navbar.Brand href="/">CS Masters 2025</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About Me</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
 
